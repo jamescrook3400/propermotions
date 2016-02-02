7fdc839356af305ad348b3638f1e8035120d79b8
jamescrook3400 <james.crook01@gmail.com>
2016-02-02 16:38:41 -0500

    Simplified shortname generator
    separated from other, less important, RADEC updating code

--- shortnamegenerator.py
+++ shortnamegenerator.py
@@ -1,9 +1,6 @@
 from BDNYCdb import BDdb
 import numpy as np
-import re 
-db=BDdb.get_db('/Users/jamescrook/Documents/Modules/BDNYC.db')
-DB_data=db.list("select names,ra,dec from sources").fetchall()
-#DA_data=np.genfromtxt('/Users/Joe/Desktop/obj_list.txt', delimiter=",", dtype=object)
+import re
 
 #Function to generate shortnames from a given name for a list of objects
 def get_shortname(name):
@@ -108,39 +105,5 @@ def get_shortname(name):
             if letter not in com:
                 shortname = ''
     return shortname
-
-def generate_DA_shortnames(DA_data):  
-    allshortnames=[]
-    allnames=[]
-    wierdones=[]
-    
-    #For objects without shortnames
-    for (name, ra, dec) in DA_data:
-        shortname = get_shortname(name)
-        allshortnames.append(shortname)
-    
-    data = np.concatenate([np.array([allshortnames]),DA_data.T]).T
-    
-    return data
-
-def generate_DB_shortnames():
-    """
-    Generates shortnames for objects in need using designations and names    
-    """
-    need_sn = db.list("select id,names,designation from sources where shortname is null or shortname=''").fetchall()
-    for source_id,names,designation in need_sn:
-      shortname = ''
-      if designation: shortname = get_shortname(designation)
-      if names and not shortname: shortname = get_shortname(names)
-      if shortname: 
-        response = raw_input("Update source {} {} {} with shortname {}?[y,n] ".format(source_id,designation,names,shortname))
-        if response=='y': db.list("update sources set shortname='{}' where id={}".format(shortname,source_id))
-        else: print 'Not updated.'
-    
-def fix_RA(data):       
-    for shortname,name,ra,dec in data:
-      try:
-        print db.list("select ra,dec from sources where shortname='{}'".format(shortname)).fetchone(), (ra,dec)
-        db.dict("update sources set ra={}, dec={} where shortname='{}'".format(float(ra),float(dec),shortname)) 
-      except: print 'No object with shortname '+shortname                       
+                       
                                                 
