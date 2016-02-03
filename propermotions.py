0baab906dd281242f3935c99a2d8054cd27016d7
jamescrook3400 <james.crook01@gmail.com>
2016-02-02 19:03:37 -0500

    Update
    updated propermotions code to match changes to the database code

--- propermotions.py
+++ propermotions.py
@@ -1,14 +1,11 @@
-from BDNYCdb import BDdb
+from astrodbkit.astrodbkit import astrodb
 import numpy as np
 import RADEC 
-db=BDdb.get_db('/Users/jamescrook/Documents/Modules/BDNYC.db')
+db=astrodb.get_db('/Users/jamescrook/Documents/Modules/BDNYC.db')
 
 #ADD COMMENTS and generate shortnames and update that way
-#run g
-old=np.genfromtxt('/Users/jamescrook/Documents/Modules/faherty08.txt', delimiter='|', dtype=object)
-new=np.genfromtxt('/Users/jamescrook/Documents/Modules/fahertydata.txt', delimiter=',', dtype=object)
-newpublications=[[25,208],[50,212],[49,213],[53,214],[59,215],[342,217],[94,221],[110,223],[334,224],[118,225],["NULL",209],["NULL", 210],["NULL",216],["NULL",218],["NULL",219],["NULL",222],["NULL",227],["NULL", 229],[193,231],[168,232],[193,233],[300,234],[346,235],[514,228],[360,206],[396,205],[463, 204],["NULL",202],["NULL",247],["NULL",246],["NULL",245],[467,241],["NULL",238],[228,236],[244,237],[257,242],[263,244],[259,243],[255,240], [166,230]]
-DB_data=db.dict("select * from sources").fetchall()
+newpublications=[[25,208],[50,212],[49,213],[53,214],[59,215],[342,217],[94,221],[110,223],[334,224],[118,225],[591,209],[590, 210],['',216],['',218],[290,219],[592,222],['',227],['', 229],[193,231],[168,232],[193,233],[300,234],[346,235],[514,228],[360,206],[396,205],[463, 204],['',202],['',247],['',246],['',245],[467,241],['',238],[228,236],[244,237],[257,242],[263,244],[259,243],[255,240], [166,230]]
+DB_data=db.query("select * from sources", fmt='dict')
 global new, old
 def generate_sourceid(data):  
     allids=[]
@@ -31,14 +28,16 @@ def generate_sourceid(data):
     print data
     
 #adds new faherty data
-def add_new_data(new):
-    db.add_data('/Users/jamescrook/Documents/Modules/fahertydata.txt', 'proper_motions', delimiter = ',', multiband=False)
+def add_new_data():
+    db.add_data('new.txt', 'proper_motions', delimiter=',')
     
 #adds old faherty data
-def add_old_data(old):
-    db.add_data('/Users/jamescrook/Documents/Modules/faherty08.txt', 'proper_motions', delimiter='|', multiband=False)
+def add_old_data():
+    db.add_data('old.txt', 'proper_motions', delimiter='|')
 
 #updates publication IDs
 #FOR SOME REASON THIS ISNT UPDATING STUFF?
 def update_publications(newpublications):
-    for old,new in newpublications: db.modify("update proper_motions set publication_id={} where publication_id={}".format(new,old))
+    for new,old in newpublications:
+      if new and old: db.modify("update proper_motions set publication_id={} where publication_id={}".format(new,old))
+      else: db.modify("update proper_motions set publication_id=NULL where publication_id={}".format(old))
